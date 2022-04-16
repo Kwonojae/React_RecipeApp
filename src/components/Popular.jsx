5b31b3cc69ac69176ae15a330d3545ff81da69b1
Ojea <rnjsdhwo1325@gmail.com>
2022-04-17 01:29:31 +0900

    Category complete

---
+++ src/components/Popular.jsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+
+const Popular = () => {
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPupular();
+  }, []);
+
+  const getPupular = async () => {
+    const check = localStorage.getItem("popular"); ////getItem (popular) 키에 데이터 읽기
+    /*
+    sessionStorage:는 페이지의 세션이 유지되는 동안 사용할 수 있는 각 origin별로 별도의 스토리지를 관리합니다. (페이지 리로딩 및 복원을 포함한, 브라우저가 열려있는 한 최대한 긴 시간 동안)
+    localStorage:도 같은 일을 하지만, 브라우저가 닫히거나 다시 열리더라도 유지합니다.
+    */
+
+    if (check) {
+      setPopular(JSON.parse(check)); //popular 러를 꺼내옴
+      //   JSON.parse() 메서드는 JSON 문자열의 구문을 분석하고, 그 결과에서 JavaScript 값이나 객체를 생성합니다.
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      );
+      const data = await api.json();
+
+      localStorage.setItem("popular", JSON.stringify(data.recipes)); //setItem (popular) 키에 데이터 쓰기
+      //JSON.stringify() 메서드는 JavaScript 값이나 객체를 JSON 문자열로 변환합니다
+      setPopular(data.recipes);
+      console.log(data.recipes);
+    }
+  };
+  return (
+    <div>
+      <Wrapper>
+        <h3>Popular Picks</h3>
+        {/* 이미지 Splide  */}
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+    </div>
+  );
+};
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+
+  //그래디언트(gradient)란 둘 이상의 색 사이의 색상 표현을 부드럽게 전환해주는 효과를 의미합니다.
+`;
+
+export default Popular;
